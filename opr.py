da6bfc9729ab10073538bad0c416a3583a751f33
281developer <dave.cowden@gmail.com>
2024-12-27 20:23:27 -0500

    fixing

--- opr.py
+++ opr.py
@@ -1,25 +1,19 @@
-# calculates opr from a set of matches
 import pandas as pd
 from motherduck import con
 import numpy as np
 
 
-def calculate_opr_ccwm_dpr ( matches:pd.DataFrame) -> pd.DataFrame:
+def calculate_opr_ccwm_dpr(matches:pd.DataFrame) -> pd.DataFrame:
 
-    team_list = set(
-        matches.select([
-            'red_0_id','red_1_id','blue_0_id','blue_1_id'
-        ]).unpivot(value_name='id').get_column('id')
-    )
+    #get the unique list of teams
+    team_list = pd.unique(matches[['red1','red2','red3','blue1','blue2','blue3']].values.ravel('K'))
+    print("Unique Teams: One column, num rows= Nteams ")
 
-    # courtesy: https://github.com/owsorber/FTC_OPR_Calculator/blob/master/OPR.py
-    # i'm sure there's a more elegant way to do this!
-    # in particular by melting the wide format to a list of matches
     M=[]
-    for match in matches.iter_rows(named=True):
+    for idx,match in matches.iterrows():
         r=[]
         for team in team_list:
-            if match['red_0_id'] == team or match['red_1_id'] == team:
+            if match['red1'] == team or match['red2'] == team or match['red3'] == team:
                 r.append(1)
             else:
                 r.append(0)
@@ -27,46 +21,50 @@ def calculate_opr_ccwm_dpr ( matches:pd.DataFrame) -> pd.DataFrame:
 
         b = []
         for team in team_list:
-            if match['blue_0_id'] == team or match['blue_1_id'] == team:
+            if match['blue1'] == team or match['blue2'] == team or match['blue3'] == team:
                 b.append(1)
             else:
                 b.append(0)
         M.append(b)
 
-    scores= []
-    margins= []
-
-    for match in matches.iter_rows(named=True):
-        scores.append([match['red_score']])
-        scores.append([match['blue_score']])
-        margins.append([match['red_score'] - match['blue_score']])
-        margins.append([match['blue_score'] - match['red_score']])
+    combined = []
+    for index,match in matches.iterrows():
+        combined.extend([
+            [match['red_score'],match['red_score'] - match['blue_score']],
+            [match['blue_score'],match['blue_score'] - match['red_score'] ]
+        ])
 
     m_m = np.matrix(M)
-    s_m = np.matrix(scores)
-    c_m = np.matrix(margins)
+    c_c = np.matrix(combined)
 
     pseudo_inverse = np.linalg.pinv(m_m)
+    all = np.matmul(pseudo_inverse,c_c)
 
-    oprs = np.matmul(pseudo_inverse,s_m)
-    ccwms = np.matmul(pseudo_inverse,c_m)
-
-
-    def get_matrix_into_one_list(m):
+    def get_matrix_into_one_list(m,):
         return np.reshape(m,(1,-1)).tolist()[0]
 
+    results_2 = pd.DataFrame(all,columns=['opr','ccwm'])
+    teams_2 = pd.DataFrame(team_list,columns=['team_id'])
+    results_all = pd.concat([teams_2, results_2], axis=1)
 
-    results = pl.DataFrame( {
-        'team_id': list(team_list),
-        'opr' : get_matrix_into_one_list(oprs),
-        'ccwm': get_matrix_into_one_list(ccwms)
-    }).with_columns(
-        (pl.col('opr') - pl.col('ccwm')).alias('dpr')
-    ).sort(['opr'],descending=True)
-    return results
+    """
+            team_id        opr       ccwm
+        16     1771  59.877861  40.854197
+        4      2974  49.007513  27.565074
+        19      343  39.377108  11.797834
+        ....    
+    """
+    return results_all.sort_values(by=['opr'],ascending=False)
+
+
+def get_match_data():
+    matches = con.sql("select * from frc_2025.tba.matches where event_key = '2024gacmp'").df()
+    return matches[ ['red1','red2','red3','blue1','blue2','blue3','red_score','blue_score']]
+
 
 if __name__  == '__main__':
-    #matches = md.get_matches().filter(pl.col('event_id') == 53463)
-    matches = con.sql("select * from frc_2025.tba.matches where event_key = '2024gacmp'").pd()
+    matches = get_match_data()
+    print(matches)
     r = calculate_opr_ccwm_dpr(matches)
+    print("Results:")
     print(r)
