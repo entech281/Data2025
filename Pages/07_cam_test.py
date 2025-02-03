94b3171c571cfa35b18ad6731bbe3829ed562962
tadpoleAngel <opc4775@gmail.com>
2025-02-03 18:41:13 -0500

    cam input

--- Pages/07_cam_test.py
+++ Pages/07_cam_test.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 from motherduck import con
+from PIL import Image
+import io
 
 st.set_page_config(layout="wide")
 
@@ -8,7 +10,17 @@ st.title("Camera Test")
 
 cam_input = st.camera_input(label="Camera Test")
 
+
 if cam_input:
     st.image(cam_input)
     binary_data = cam_input.read()
-    st.image(binary_data)
+
+    con.execute("UPDATE cam_test SET img = ? WHERE id = 1;", [binary_data])
+    st.write("Successfully inserted image into database")
+    
+    st.write("Retrieving image from database")
+    retrieved_image = con.sql("SELECT img FROM cam_test WHERE id = 1").df().iloc[0, 0]
+    retrieved_image = Image.open(io.BytesIO(retrieved_image))
+    st.image(retrieved_image)
+
+    
