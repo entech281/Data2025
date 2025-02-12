2c633bc7e6fa154b674ceb8f64c894e40ec9d35e
tadpoleAngel <opc4775@gmail.com>
2025-02-11 19:07:12 -0500

    team selector pills

--- pages/07_team_selector.py
+++ pages/07_team_selector.py
@@ -30,7 +30,7 @@ st.title("Team Ranking Based on Selected Characteristics")
 
 # Select characteristics
 available_chars = [col for col in df.columns if col != "team_id"]
-selected_chars = st.multiselect("Select Characteristics", available_chars, default=['score_z','foul_count_z'])
+selected_chars = st.pills("Select Characteristics", options=available_chars, default=['score_z','foul_count_z'], selection_mode="multi")
 
 if len(selected_chars) == 0 :
     st.write("Select at least one characteristic to proceed.")
