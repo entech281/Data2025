c61e3d4326eddb395565432da6aef6adad38de9e
281developer <dave.cowden@gmail.com>
2025-03-01 13:33:08 -0500

    force numpy version

--- opr3.py
+++ opr3.py
@@ -88,7 +88,6 @@ def add_zscores(df:pd.DataFrame, cols:list[str]) -> pd.DataFrame:
 
     new_df = df.copy()
     for c in cols:
-        print(f"computing zscore for col {c} value {df[c]}")
         new_df[c + "_z"] = zscore(df[c])
     return new_df
 
