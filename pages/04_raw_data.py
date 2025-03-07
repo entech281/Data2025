82202190cbdd3f24c5da3874cb26584c46f26208
tadpoleAngel <opc4775@gmail.com>
2025-03-07 15:39:58 -0500

    fixed and added to raw data

--- pages/04_raw_data.py
+++ pages/04_raw_data.py
@@ -1,8 +1,11 @@
+from io import BytesIO
 import streamlit as st
 import cached_data
 from motherduck import con
 import base64
 from pages_util.event_selector import event_selector
+import pandas as pd
+import struct
 
 st.set_page_config(layout="wide")
 
@@ -38,9 +41,11 @@ matches= cached_data.get_matches_for_event(selected_event)  #this one is not exe
 rankings = cached_data.get_tba_oprs_and_ranks_for_event(selected_event)
 
 pit = con.sql("SELECT * FROM scouting.pit").df()
+match_scouted = con.sql("SELECT * FROM scouting.matches").df()
 
 # Format images in pit data
 if 'auto_route' in pit.columns:
+    pit['auto_route'] = pit['auto_route'].apply(lambda item: bytearray(struct.pack("f", item) if type(item) is float else item))
     pit['auto_route'] = pit['auto_route'].apply(image_formatter)
 
 
@@ -55,3 +60,5 @@ st.dataframe(rankings,column_config=display_config)
 st.header(f"Pit ({len(pit)})")
 st.markdown(pit.to_html(escape=False), unsafe_allow_html=True)
 
+st.header(f"Match Scouted ({len(match_scouted)}) This is not event Specific")
+st.dataframe(match_scouted,column_config=display_config)
