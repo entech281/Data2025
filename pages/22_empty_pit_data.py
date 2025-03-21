825f3d6573c02ae556f42c213c2b4efc1d7aa261
Paul <paulmanavalan1@gmail.com>
2025-03-21 17:03:43 -0400

    added two seperate datatables

--- pages/22_empty_pit_data.py
+++ pages/22_empty_pit_data.py
@@ -5,13 +5,21 @@ from motherduck import con
 st.set_page_config(layout="wide")
 st.title("Empty Pit Data")
 
-st.subheader("Missing Data:")
+st.subheader("Missing Picture:")
 
 all_data = con.sql("""
                    select team_number, author, notes,created_at  from scouting.pit
 where OCTET_LENGTH(auto_route) = 0 or OCTET_LENGTH(robot_picture) = 0
-or created_at < NOW() - INTERVAL '7' DAY
 order by team_number asc;
                    """
                    ).df()
 st.dataframe(all_data )
+
+st.subheader("Not Updated Recently:")
+
+all_data = con.sql("""
+                     select team_number, author, notes,created_at  from scouting.pit
+    where  created_at < NOW() - INTERVAL '7' DAY
+                   """
+                     ).df()
+st.dataframe(all_data)
